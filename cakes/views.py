0336a83c748595a3f735216b055c83940b5d5205
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-30 00:18:56 +0400

    views: query for brands and categories outside of view functions
    This is so we can render the navbar dropdowns.

--- cakes/views.py
+++ cakes/views.py
@@ -4,9 +4,14 @@ from flask import render_template, request, redirect, url_for, flash
 
 from cakes import app
 from cakes.database import session
-from cakes.models import Brand, Category, SubCategory, Item, Notes
+from cakes.models import Brand, Category, SubCategory, Product, Notes
 
+
+brands = session.query(Brand).order_by(Brand.name.asc()).all()
+categories = session.query(Category).order_by(Category.name.asc()).all()
 
 @app.route("/")
 def home():
-    return "Hello Cakes"
+    products = session.query(Product).all()
+    return render_template("home.html", brands=brands, products=products,
+                                        categories=categories)
