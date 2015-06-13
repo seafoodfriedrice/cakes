6008576393e82bffe9a72d6a0f35fafa2b1dc0da
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-06-14 02:00:29 +0400

    config: set WTF_CSRF_ENABLED to true

--- cakes/config.py
+++ cakes/config.py
@@ -3,4 +3,5 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/cakes"
     DEBUG = True
+    WTF_CSRF_ENABLED = True
     SECRET_KEY = "848e462678fe13b9c3ae5c15d902ad15"
