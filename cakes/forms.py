48dc48a6e85dabcab6d9b67feb58e298ffbde023
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-06-14 01:59:53 +0400

    Create forms.py with ProductForm() which uses Flask-WTForms extension

---
+++ cakes/forms.py
@@ -0,0 +1,17 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, IntegerField
+from wtforms import DecimalField, SelectField, TextAreaField
+from wtforms.validators import Required, Length, Optional
+
+from cakes.database import session
+
+
+class ProductForm(Form):
+    category = SelectField(u'Category', coerce=int, validators=[Required()])
+    brand = SelectField(u'Brand', coerce=int, validators=[Required()])
+    name = StringField(u'Product Name', validators=[Required()])
+    color = StringField(u'Color', validators=[Length(max=48)])
+    quantity = IntegerField(u'Quantity', default=0, validators=[Optional()])
+    price = DecimalField(u'Price', default=0.0, validators=[Optional()])
+    favorite = BooleanField(u'Favorite', default=False, validators=[Optional()])
+    notes = TextAreaField(u'Product Notes', validators=[Optional()])
