e8ca06bc43b95ffda6c2e2b9348d734dfa4d8744
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-06-14 23:09:18 +0400

    forms: use QuerySelectField to get list of brands and categories

--- cakes/forms.py
+++ cakes/forms.py
@@ -1,14 +1,21 @@
 from flask.ext.wtf import Form
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms import StringField, BooleanField, IntegerField
 from wtforms import DecimalField, SelectField, TextAreaField
 from wtforms.validators import Required, Length, Optional
 
 from cakes.database import session
+from cakes.models import Category, Brand
 
+def category_names():
+    return session.query(Category)
+
+def brand_names():
+    return session.query(Brand)
 
 class ProductForm(Form):
-    category = SelectField(u'Category', coerce=int, validators=[Required()])
-    brand = SelectField(u'Brand', coerce=int, validators=[Required()])
+    category = QuerySelectField(get_label='name', query_factory=category_names)
+    brand = QuerySelectField(get_label='name', query_factory=brand_names)
     name = StringField(u'Product Name', validators=[Required()])
     color = StringField(u'Color', validators=[Length(max=48)])
     quantity = IntegerField(u'Quantity', default=1, validators=[Optional()])
