f0133bdd248d7d3bcf2c472fce8e88349a6d4c65
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-12-12 19:45:33 +0400

    init: wrap project inside SQLAlchemy Flask extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 import os
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "app.config.DevelopmentConfig")
 app.config.from_object(config_path)
+
+db = SQLAlchemy(app)
+db.create_all()
 
 from . import views
 from . import filters
