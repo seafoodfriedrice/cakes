84a31b535a4ca70ccc88e7b8a98d6900c28f5032
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-12-12 19:45:04 +0400

    models: change to use db object rather than declarative_Base

--- app/models.py
+++ app/models.py
@@ -1,33 +1,28 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Float, Integer, String, ForeignKey, Boolean
 from flask.ext.login import UserMixin
+from app import db
 
-from app.database import Base, engine, session
 
-
-class Brand(Base):
+class Brand(db.Model):
     __tablename__ = "brands"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(48), nullable=False, unique=True)
-    products = relationship('Product', backref='brand')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(48), nullable=False, unique=True)
+    products = db.relationship('Product', backref='brand')
 
 
-class Product(Base):
+class Product(db.Model):
     __tablename__ = "products"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(48))
-    color = Column(String(48))
-    quantity = Column(Integer, default=int(1))
-    price = Column(Float)
-    category_id = Column(Integer, ForeignKey('categories.id'))
-    sub_category_id = Column(Integer, ForeignKey('sub_categories.id'))
-    brand_id = Column(Integer, ForeignKey('brands.id'))
-    notes = Column(String(2048))
-    favorite = Column(Boolean, default=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(48))
+    color = db.Column(db.String(48))
+    quantity = db.Column(db.Integer, default=int(1))
+    price = db.Column(db.Float)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    sub_category_id = db.Column(db.Integer, db.ForeignKey('sub_categories.id'))
+    brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'))
+    notes = db.Column(db.String(2048))
+    favorite = db.Column(db.Boolean, default=True)
 
     def as_dictionary(self):
         product = {
@@ -43,30 +38,27 @@ class Product(Base):
         return product
 
 
-class Category(Base):
+class Category(db.Model):
     __tablename__ = "categories"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(24), unique=True)
-    sub_categories = relationship('SubCategory', backref='category')
-    products = relationship('Product', backref='category')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24), unique=True)
+    sub_categories = db.relationship('SubCategory', backref='category')
+    products = db.relationship('Product', backref='category')
 
 
-class SubCategory(Base):
+class SubCategory(db.Model):
     __tablename__ = "sub_categories"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(24), unique=True)
-    category_id = Column(Integer, ForeignKey('categories.id'))
-    products = relationship('Product', backref='sub_category')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24), unique=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    products = db.relationship('Product', backref='sub_category')
 
 
-class User(Base, UserMixin):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(32), unique=True)
-    password = Column(String(128))
-
-
-Base.metadata.create_all(engine)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(128))
