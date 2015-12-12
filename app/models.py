b4e90ec717e53558ecb0acb7dbfdf3313fb439b3
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-12-12 19:56:44 +0400

    models: add additional fields to Product class and add __repr__ for all

--- app/models.py
+++ app/models.py
@@ -9,6 +9,9 @@ class Brand(db.Model):
     name = db.Column(db.String(64), nullable=False, unique=True)
     products = db.relationship('Product', backref='brand')
 
+    def __repr__(self):
+        return "<Brand {!r}>".format(self.name)
+
 
 class Product(db.Model):
     __tablename__ = "products"
@@ -18,11 +21,13 @@ class Product(db.Model):
     color = db.Column(db.String(64))
     quantity = db.Column(db.Integer, default=int(1))
     price = db.Column(db.Float)
+    favorite = db.Column(db.Boolean, default=False)
+    stars = db.Column(db.Float)
+    review = db.Column(db.Text)
+    notes = db.Column(db.Text)
+    brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     sub_category_id = db.Column(db.Integer, db.ForeignKey('sub_categories.id'))
-    brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'))
-    notes = db.Column(db.Text))
-    favorite = db.Column(db.Boolean, default=True)
 
     def as_dictionary(self):
         product = {
@@ -37,6 +42,9 @@ class Product(db.Model):
         }
         return product
 
+    def __repr__(self):
+        return "<Product {!r}>".format(self.name)
+
 
 class Category(db.Model):
     __tablename__ = "categories"
@@ -45,6 +53,9 @@ class Category(db.Model):
     name = db.Column(db.String(64), unique=True)
     sub_categories = db.relationship('SubCategory', backref='category')
     products = db.relationship('Product', backref='category')
+
+    def __repr__(self):
+        return "<Category {!r}>".format(self.name)
 
 
 class SubCategory(db.Model):
@@ -55,6 +66,9 @@ class SubCategory(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     products = db.relationship('Product', backref='sub_category')
 
+    def __repr__(self):
+        return "<SubCategory {!r}>".format(self.name)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
@@ -62,3 +76,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
     password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return "<User {!r}>".format(self.username)
+
